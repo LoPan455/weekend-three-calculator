7b54b9e1c8dc30fb7750d60cd8310a8be10b0715
Lopan455 <tjohander@gmail.com>
2017-02-25 10:19:10 -0600

    All buttons working properly in the client

--- server/routes/calculator.js
+++ server/routes/calculator.js
@@ -3,29 +3,42 @@ var router = express.Router();
 var calulatorOutput; //holds the result of the calulation
 
 router.post('/division',function(req,res){
-  res.sendStatus(200);
-  console.log('divison happened');
-})
+    var response = req.body;
+    console.log('the POST request consisted of: ',response,' let me do some work on that');
+    calculatedOutput = parseInt(response.x) / parseInt(response.y);
+    console.log(calculatedOutput);
+    res.send(calculatedOutput+'');
+
+}) // end /division
 
 router.post('/addition',function(req,res){
-  
   var response = req.body;
   console.log('the POST request consisted of: ',response,' let me do some work on that');
   calculatedOutput = parseInt(response.x) + parseInt(response.y);
   console.log(calculatedOutput);
   res.send(calculatedOutput+'');
 
-});
+}); // end /addition
 
 router.post('/subtraction',function(req,res){
-  res.sendStatus(200);
+  var response = req.body;
+  console.log('the POST request consisted of: ',response,' let me do some work on that');
+  calculatedOutput = parseInt(response.x) - parseInt(response.y);
+  console.log(calculatedOutput);
+  res.send(calculatedOutput+'');
   console.log('subtraction happened');
-});
+}); // end /subtraction
 
-router.post('/multiplication',function(res,req){
-  res.sendStatus(200);
+router.post('/multiplication',function(req,res){
+  console.log('req.body = ',req.body);
+  var response = req.body;
+
+  console.log('the POST request consisted of: ',response,' let me do some work on that');
+  calculatedOutput = parseInt(response.x) * parseInt(response.y);
+  console.log(calculatedOutput);
+  res.send(calculatedOutput+'');
   console.log('multiplication happened');
-})
+}); // end /multiplication
 
 
 
