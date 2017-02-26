d88f791ef5c8cd4a81e75c5569752fd025a7b9d3
Lopan455 <tjohander@gmail.com>
2017-02-26 09:21:52 -0600

    Pre Bootstrap Install

--- server/app.js
+++ server/app.js
@@ -4,7 +4,11 @@ var express = require('express'); //brings in the Express module
 var app = express(); //this is a function that RETURNS an OBJECT
 var bodyParser = require('body-parser');
 var calculator = require('./routes/calculator.js');
-app.use(express.static('server/public')); // this methods says, 'is this request for a static file?  If so, I will look in '/server/public'
+
+
+app.use(express.static('public')); // this methods says, 'is this request for a static file?  If so, I will look in '/server/public'
+
+
 app.use(bodyParser.urlencoded({extended: true})); //if we want to parse the body of a response!
 
 
